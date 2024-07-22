6d71ceda4951115d84e7900c16dd534ce34a7eed
Goldenkiuren <augustomgrohmann@gmail.com>
2024-07-21 21:36:15 -0300

    Imagem inicial e tela cheia

--- core/src/main/java/menu/java/menu.java
+++ core/src/main/java/menu/java/menu.java
@@ -15,14 +15,14 @@ public class menu extends ApplicationAdapter {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("logo.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, 0, 0);
         batch.end();
     }
 
